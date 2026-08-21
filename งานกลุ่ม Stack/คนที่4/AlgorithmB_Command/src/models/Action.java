5225b70ad2c847dfc72f3223ef89049484b37310
s68122250099-art <s68122250099@ssru.ac.th>
2026-08-21 15:54:23 +0700

    Update Action.java

--- งานกลุ่ม Stack/คนที่4/AlgorithmB_Command/src/models/Action.java
+++ งานกลุ่ม Stack/คนที่4/AlgorithmB_Command/src/models/Action.java
@@ -1,13 +1,18 @@
 package models;
 
+import java.time.LocalDateTime;
+
 public class Action {
-    public enum Type { INSERT, DELETE, REPLACE }
-    
+    public enum Type {
+        INSERT, DELETE, REPLACE
+    }
+
     private String id;
     private Type type;
     private int position;
     private String oldText;
     private String newText;
+    private LocalDateTime timestamp;
 
     public Action(String id, Type type, int position, String oldText, String newText) {
         this.id = id;
@@ -15,11 +20,30 @@ public class Action {
         this.position = position;
         this.oldText = oldText;
         this.newText = newText;
+        this.timestamp = LocalDateTime.now();
     }
 
-    public String getId() { return id; }
-    public Type getType() { return type; }
-    public int getPosition() { return position; }
-    public String getOldText() { return oldText; }
-    public String getNewText() { return newText; }
+    public String getId() {
+        return id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getOldText() {
+        return oldText;
+    }
+
+    public String getNewText() {
+        return newText;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 }
